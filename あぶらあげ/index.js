b406b1325e8a48ccc8e0f0fc1c9375de3ecd1a9a
Rento <109392019+rento2177@users.noreply.github.com>
2023-05-27 15:07:44 +0900

    Update index.js

--- あぶらあげ/index.js
+++ あぶらあげ/index.js
@@ -1,1 +1,11 @@
+let id = localStorage.getItem("ID");
+let ps;
+if (id == null) {
+    id = Math.random().toString(36).slice(-9);
+    localStorage.setItem("ID", id)
+}
+console.log(id);
 
+if (ps != null) {
+    id = id + ", kkk";
+}
